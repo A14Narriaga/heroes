70f595fb4a92f607645e143190095d5947da5efe
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2021-11-05 01:43:19 -0600

    🐛 FIX 🔹App, Navbar, Home, About and Heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// Modules
+import { NgModule } from '@angular/core'
+import {
+	RouterModule,
+	Routes,
+} from '@angular/router'
 
-const routes: Routes = [];
+// Components
+import { HomeComponent } from './components/home/home.component'
+import { HeroesComponent } from './components/heroes/heroes.component'
+import { AboutComponent } from './components/about/about.component'
+
+const routes: Routes = [
+	{
+		path: 'home',
+		component: HomeComponent,
+	},
+	{
+		path: 'heroes',
+		component: HeroesComponent,
+	},
+	{
+		path: 'about',
+		component: AboutComponent,
+	},
+	{
+		path: '**',
+		redirectTo: 'home',
+	},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+// Modules
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module'
+import { ServiceWorkerModule } from '@angular/service-worker'
+
+// Components
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './components/shared/navbar/navbar.component'
+import { HomeComponent } from './components/home/home.component'
+import { AboutComponent } from './components/about/about.component'
+import { HeroesComponent } from './components/heroes/heroes.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		HomeComponent,
+		AboutComponent,
+		HeroesComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ServiceWorkerModule.register(
+			'ngsw-worker.js',
+			{
+				enabled: environment.production,
+				registrationStrategy:
+					'registerWhenStable:30000',
+			}
+		),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
